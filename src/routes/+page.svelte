41c52f16e427a523c99589a8f4c840291c35e547
KaikyBraga <kaiky.braga06221607@gmail.com>
2025-07-15 23:53:25 -0300

    feat : initial page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import './base.css';
+    import './initial-page.css';
+    import './presentation.css';
+    
+</script>
+
+<!-- Only my name to scroll -->
+<div class="initial-page">
+    <div class="text">
+        <h1>Kaiky Braga</h1>
+        <p>DATA SCIENCE STUDENT</p>
+    </div>
+
+    <div class="scroll-indicator">
+        <div class="arrows">
+            <svg class="arrow-icon" viewBox="0 0 24 24" fill="none">
+                <path d="M6 9l6 6 6-6" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </div>
+        <div class="text">Scroll Down</div>
+    </div>
+</div>
+
+<!-- Who I am and Contacts -->
+<div class="presentation"> 
+</div>
+
+<!-- What I am studying now -->
+<div class="studies">    
+</div>
+
+<!-- Programming Languages -->
+<div class="languages">
+</div>
+
+<!-- Courses -->
+<div class="courses">    
+</div>
+
+<!-- Complementaries informations -->
+<div class="complementaries">
+</div>
+
+<!-- Projects -->
+<div class="projects">
+</div>
