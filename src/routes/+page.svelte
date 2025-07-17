8074869e21acd4c5e3b42407543036106c8dbf9f
KaikyBraga <kaiky.braga06221607@gmail.com>
2025-07-16 23:08:43 -0300

    add : nova página

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,11 @@
     import './base.css';
     import './initial-page.css';
     import './presentation.css';
+    import './studies.css';
+    import './languages.css';
+    import './courses.css';
+    import './complementaries.css';
+    import './projects.css';
     
 </script>
 
@@ -24,11 +29,84 @@
 
 <!-- Who I am and Contacts -->
 <div class="presentation"> 
+    <div class="left-presentation">
+        <div class="text-presentation">
+            <h1>Olá, eu sou Kaiky Braga!</h1>
+            <hr>
+            <p><b>Um graduando em Ciência de Dados e Inteligência Artificial pela Fundação Getulio Vargas (FGV)</b>, com grande interesse em aprendizado de máquina, estatística (frequentista e bayesiana) e análise de dados. Estou constantemente em busca de novos aprendizados e desafios que ampliem meu conhecimento técnico e prático.</p>
+        </div>
+
+        <div class="social-links">
+            <!-- E-mail -->
+            <b>Contato:</b>
+            <a href="mailto:kaiky.braga06221607@gmail.com" class="email-link">
+                kaiky.braga06221607@gmail.com
+            </a>
+            <div class="social-images">
+                <!-- Linkedin -->
+                <a href="https://github.com/KaikyBraga" target="_blank">
+                    <img src="linkedin.png" alt="linkedin">
+                </a>
+
+                <!-- GitHub -->
+                <a href="https://www.linkedin.com/in/kaiky-braga-8a84962aa/" target="_blank">
+                    <img src="github.png" alt="github">
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="right-presentation">
+        <div class="profile-container">
+            <div class="profile-border"></div>
+            <img src="kaiky_picture.jpg" alt="Kaiky Braga" class="profile-image">
+        </div>
+    </div>
 </div>
 
 <!-- What I am studying now -->
 <div class="studies">    
+    <div class="studies-wrapper">
+        <h1 class="study-title-principal">Estudos / Interesses</h1>
+
+        <div class="study-block">
+            <div class="studies-row">
+                <div class="studies-col" id="studies-col1">
+                    <div class="study-heading">
+                        <div class="study-icon"></div>
+                        <h2 class="study-title">INTELIGÊNGIA ARTIFICIAL</h2>
+                    </div>
+                    <div class="study-text">Tenho experiência na exploração de técnicas de Machine Learning, incluindo aprendizado supervisionado, não supervisionado e aprendizado por reforço. Já realizei projetos diretamente com automatizações utilizando LLMs. Além disso, desenvolvi e participei de trabalhos voltados para clusterização de dados, para identificar padrões e segmentações relevantes em conjuntos de dados reais.</div>
+                </div>
+                <div class="studies-col" id="studies-col2">
+                    <div class="study-heading">
+                        <div class="study-icon"></div>
+                        <h2 class="study-title">MATEMÁTICA</h2>
+                    </div>
+                    <div class="study-text">Tive a oportunidade de aprofundar conhecimentos em diversas e fundamentais áreas da matemática. Isso incluiu a realização de trabalhos envolvendo Modelos Lineares Generalizados na área de Estatística, a aplicação prática de Álgebra Linear em problemas reais, além do uso integrado de conceitos de probabilidade e estatística, tanto na abordagem Bayesiana quanto na Frequentista, para a resolução de desafios complexos e desenvolvimento de modelos matemáticos robustos.</div>
+                </div>
+            </div>
+
+            <div class="studies-row">
+                <div class="studies-col" id="studies-col3">
+                    <div class="study-heading">
+                        <div class="study-icon"></div>
+                        <h2 class="study-title">ANÁLISES EXPLORATÓRIAS</h2>
+                    </div>
+                    <div class="study-text">Análise e limpeza de dados, bem como a geração de insights por meio de técnicas estatísticas, são pilares centrais do meu conjunto de habilidades. Além disso, tenho me dedicado a transformar dados complexos em narrativas visuais claras e acessíveis por meio de visualizações eficazes, com o objetivo de facilitar a compreensão para públicos diversos, independentemente do nível técnico.</div>
+                </div>
+                <div class="studies-col" id="studies-col4">
+                    <div class="study-heading">
+                        <div class="study-icon"></div>
+                        <h2 class="study-title">ARQUITETURA DE PROJETOS</h2>
+                    </div>
+                    <div class="study-text">Tive a oportunidade de aplicar boas práticas de desenvolvimento de software, com foco em otimização de algoritmos, escalabilidade de aplicações e escrita de código limpo e eficiente. Além disso, adquiri conhecimentos em programação em nuvem em nível básico e em modelagem de banco de dados, compreendendo a importância de estruturas bem definidas para garantir desempenho e integridade dos sistemas.</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
 
 <!-- Programming Languages -->
 <div class="languages">
